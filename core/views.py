35d83ef5fbe6993df3292af7d66f6edfb9564a0e
Noble <neluwah@gmail.com>
2025-02-01 10:51:34 +0100

    Final Touches

--- core/views.py
+++ core/views.py
@@ -8,7 +8,7 @@ from django.utils.timezone import now
 def public_api(request):
     data = {
         "email": "neluwah@gmail.com",
-        "current_datetime": now().isoformat(),
+        "current_datetime": now().strftime("%Y-%m-%dT%H:%M:%SZ"),
         "github_url": "https://github.com/nobleenia/hng-api.git"
     }
     return JsonResponse(data, json_dumps_params={'indent': 2})
